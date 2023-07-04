49b5ec1e844badf3d684ced303e7f6a6bb268252
Leticia Moraes <leticiaemilymoraes14@gmail.com>
2023-07-04 14:19:30 -0300

    Adicionando o Desafio da Faculdade ACME em C

--- Desafios/FaculdadeACME/FaculdadeACME.c
+++ Desafios/FaculdadeACME/FaculdadeACME.c
@@ -11,18 +11,18 @@ int main()
 	int curso, adm=800, ads=1300, enfer=850, dire=900, desc=0,desconto, precofinal, porcentagemfinal, precofinal5dia,pre, porcentagem5dia;
 	char beneficios,repetir;
 	
-	printf("Olá, somos a faculdade ACME estamos aqui para fazer seu cadastro para nossa faculdade \3\3\3\n");
-	printf("Para começarmos insira seu nome completo:\n");
+	printf("Ola, somos a faculdade ACME estamos aqui para fazer seu cadastro para nossa faculdade \3\3\3\n");
+	printf("Para comecarmos insira seu nome completo:\n");
 	gets (nome);
 	system ("cls");
 	//pedindo para colocar o nome e guardando na variavel nome
 	
-	printf("Ótimo agora escolha um numero de acordo com curso desejado:\n1-Administração:R$800.00\n2-Análise de Sistemas:R$1300.00\n3-Enfermagem:R$850.00\n4-Direito:R$900.00\n");
+	printf("otimo agora escolha um numero de acordo com curso desejado:\n1-Administracao:R$800.00\n2-Analise de Sistemas:R$1300.00\n3-Enfermagem:R$850.00\n4-Direito:R$900.00\n");
 	scanf("%d", &curso);
 	//pedindo para o candidato escolher o curso
 	system ("cls");
-	printf(" Descontos são aplicados nas seguintes condições:\n1-Se você é servidor público? \n2-Se você é Aluno oriundo de escola pública? \n3-Se você já tem uma graduação?\nCaso queira mais 5%% de desconto pague antes do 5° dia útil do mês\n");
-	printf(" Você se aplica em uma dessas condições? (S/N)\n");
+	printf(" Descontos sao aplicados nas seguintes condicoes:\n1-Se voce e servidor publico? \n2-Se voce e Aluno oriundo de escola publica? \n3-Se voce ja tem uma graduacao?\nCaso queira mais 5%% de desconto pague antes do 5Âº dia util do mes\n");
+	printf(" Voce se aplica em uma dessas condicoes? (S/N)\n");
 	scanf(" %c",&beneficios);
 	system ("cls");
 	if(beneficios=='s')
@@ -30,7 +30,7 @@ int main()
 		do 
 		{
 			system ("cls");
-			printf("Selecione um numero de acordo com a condição você se aplica:\n1-Se você é servidor público? \n2-Se você é Aluno oriundo de escola pública? \n3-Se você já tem uma graduação?\n");
+			printf("Selecione um numero de acordo com a condicao voce se aplica:\n1-Se voce e servidor publico? \n2-Se voce e Aluno oriundo de escola publica? \n3-Se voce ja tem uma graduacao?\n");
 			scanf("%d", &desconto);
 			if(desconto==1)
 			{
@@ -46,25 +46,25 @@ int main()
 			}
 			else
 			{
-				printf("Opção inválida\n");
+				printf("Opcao invalida\n");
 			}
-			printf(" Gostaria de selecionar mais uma condição? (S/N)\n");
+			printf(" Gostaria de selecionar mais uma condicao? (S/N)\n");
 			scanf(" %c", &repetir);
 			system ("cls");
 		}
 		while(repetir=='s');
 	}
-	//Tabela mostrando as informaçoes selecionadas
+	//Tabela mostrando as informaï¿½oes selecionadas
 	printf("nome do candidato: %s\n", nome);
 	//mostrando o nome do candidato
 	if (curso == 1)
 	{
-		printf(" Curso escolhido: Administração \n");
+		printf(" Curso escolhido: Administracao \n");
 		printf(" Valor cheio: R$800,00 \n");
 	} 
 	else if (curso == 2)
 	{
-		printf(" Curso escolhido: Análise de sistemas \n");
+		printf(" Curso escolhido: Analise de sistemas \n");
 		printf(" Valor cheio: R$1300,00 \n");
 	} 
 	else if (curso == 3)
@@ -79,9 +79,9 @@ int main()
 	}
 	else 
 	{
-		printf("Você é um idiota e não escolheu um curso válido, parabéns você não fará uma faculdade \3\3 \n");
+		printf("Voce e um idiota e nao escolheu um curso valido, parabans voce nao fara uma faculdade \3\3 \n");
 	}
-	//Estrutura decisão: Mostra o curso e o valor do curso conforme escolhido
+	//Estrutura decisï¿½o: Mostra o curso e o valor do curso conforme escolhido
 	printf(" Total do desconto:%%%d\n", desc);
 	//mostrando o desconto depois de responder o questionario
 	if (curso == 1)
@@ -110,11 +110,11 @@ int main()
 	}
 	else 
 	{
-		printf(" Você é um idiota e não escolheu um curso válido, parabéns você não fará uma faculdade \3\3 \n");
+		printf("Voce e um idiota e nao escolheu um curso valido, parabans voce nao fara uma faculdade \3\3 \n");
 	}
 	porcentagem5dia=precofinal * 5/100;
 	precofinal5dia=precofinal-porcentagem5dia;
-	printf(" Caso Você pague antes do 5° dia útil o valor pago será de: R$%d,00\n",precofinal5dia );
+	printf(" Caso Voce pague antes do 5Âº dia util o valor pago sera de: R$%d,00\n",precofinal5dia );
 	
 	system("pause");
 	
